d333240d9af6da49002030512d1b2cdd24f97051
Volha Yanusheuskaya <Volha_Yanusheuskaya@epam.com>
2024-03-27 19:36:32 +0200

    factory method pattern implementation

--- src/test/java/com/epam/taf/driver/DriverSingleton.java
+++ src/test/java/com/epam/taf/driver/DriverSingleton.java
@@ -1,12 +1,9 @@
 package com.epam.taf.driver;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import com.epam.taf.browser.BrowserFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.safari.SafariDriver;
 
 public class DriverSingleton {
     private static WebDriver driver;
@@ -26,24 +23,7 @@ public class DriverSingleton {
 
     public WebDriver getDriver() {
         if (driver == null) {
-            switch (System.getProperty("browser")) {
-                case "firefox": {
-                    WebDriverManager.firefoxdriver().setup();
-                    driver = new FirefoxDriver();
-                    break;
-                }
-                case "safari": {
-                    WebDriverManager.safaridriver().setup();
-                    driver = new SafariDriver();
-                    break;
-                }
-                default: {
-                    WebDriverManager.chromedriver().setup();
-                    driver = new ChromeDriver();
-                    break;
-                }
-            }
-            driver.manage().window().maximize();
+            driver = BrowserFactory.createInstance();
         }
         return driver;
     }
